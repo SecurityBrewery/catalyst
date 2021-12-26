ababa7e3e45ddd8e16f28c81ef8422149886b818
Jonas Plum <git@jonasplum.de>
2021-12-27 00:11:41 +0100

    Undo log changes

--- service/artifact.go
+++ service/artifact.go
@@ -2,26 +2,13 @@ package service
 
 import (
 	"context"
-	"fmt"
-	"log"
 
 	"github.com/google/uuid"
 
-	"github.com/SecurityBrewery/catalyst/database"
 	"github.com/SecurityBrewery/catalyst/generated/models"
 	"github.com/SecurityBrewery/catalyst/generated/restapi/api"
 	"github.com/SecurityBrewery/catalyst/generated/restapi/operations/tickets"
 )
-
-func (s *Service) logTicket(ctx context.Context, id int64, function string) {
-	if _, err := s.database.LogCreate(ctx, ticketID(id), function); err != nil {
-		log.Println(err)
-	}
-}
-
-func ticketID(id int64) string {
-	return fmt.Sprintf("%s/%d", database.TicketCollectionName, id)
-}
 
 func (s *Service) RunArtifact(ctx context.Context, params *tickets.RunArtifactParams) *api.Response {
 	artifact, err := s.database.ArtifactGet(ctx, params.ID, params.Name)
@@ -35,8 +22,6 @@ func (s *Service) RunArtifact(ctx context.Context, params *tickets.RunArtifactPa
 }
 
 func (s *Service) EnrichArtifact(ctx context.Context, params *tickets.EnrichArtifactParams) *api.Response {
-	go s.logTicket(ctx, params.ID, "EnrichArtifact") // TODO
-
 	return response(s.database.EnrichArtifact(ctx, params.ID, params.Name, params.Data))
 }
 
