72c332120260d2365ed460222c961a29ffca22aa
Jonas Plum <git@jonasplum.de>
2022-02-13 20:37:38 +0100

    Fix network configuration

--- busservice/busservice.go
+++ busservice/busservice.go
@@ -22,7 +22,7 @@ type busService struct {
 
 func New(apiURL, apikey, network string, catalystBus *bus.Bus, db *database.Database) error {
 
-	h := &busService{db: db, apiURL: apiURL, apiKey: apikey, catalystBus: catalystBus}
+	h := &busService{db: db, apiURL: apiURL, apiKey: apikey, network: network, catalystBus: catalystBus}
 
 	if err := catalystBus.SubscribeRequest(h.logRequest); err != nil {
 		return err
