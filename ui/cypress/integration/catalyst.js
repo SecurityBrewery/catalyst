cd12cc194c00972c7315bc041572d883202c2a48
Jonas Plum <cugu@users.noreply.github.com>
2022-04-27 22:31:45 +0200

    More cleanups

--- ui/cypress/integration/catalyst.js
+++ ui/cypress/integration/catalyst.js
@@ -1,25 +1,24 @@
-describe('Login', () => {
-    it('can login', () => {
-        cy.visit('/home');
+describe('user', () => {
+
+    beforeEach(function () {
+        cy.fixture('user_demo').then((user_demo) => {
+            this.user_demo = user_demo
+        })
+        cy.fixture('userdata_demo').then((userdata_demo) => {
+            this.userdata_demo = userdata_demo
+        })
+    })
+
+    it('open ticket', () => {
+        cy.visit('/');
 
         // login
         cy.get("#username").type("bob");
         cy.get("#password").type("bob");
         cy.get("#kc-login").click();
 
-        cy.intercept('GET', '/api/userdata/demo', {
-            statusCode: 200,
-            body: {"email": "demo@example.org", "id": "demo", "name": "Demo"},
-        })
-        cy.intercept('GET', '/api/users/demo', {
-            statusCode: 200,
-            body: {
-                "apikey": false,
-                "blocked": false,
-                "id": "demo",
-                "roles": ["admin:backup:read", "admin:backup:restore", "admin:dashboard:write", "admin:group:write", "admin:job:read", "admin:job:write", "admin:log:read", "admin:settings:write", "admin:ticket:delete", "admin:user:write", "admin:userdata:read", "admin:userdata:write", "analyst:automation:read", "analyst:currentsettings:write", "analyst:currentuser:read", "analyst:currentuserdata:read", "analyst:dashboard:read", "analyst:file", "analyst:group:read", "analyst:playbook:read", "analyst:rule:read", "analyst:settings:read", "analyst:template:read", "analyst:ticket:read", "analyst:ticket:write", "analyst:tickettype:read", "analyst:user:read", "engineer:automation:write", "engineer:playbook:write", "engineer:rule:write", "engineer:template:write", "engineer:tickettype:write"]
-            },
-        })
+        cy.intercept('GET', '/api/userdata/demo', { statusCode: 200, body: this.userdata_demo })
+        cy.intercept('GET', '/api/users/demo', { statusCode: 200, body: this.user_demo })
 
         cy.origin('http://localhost', () => {
             cy.visit('/tickets');
@@ -32,8 +31,10 @@ describe('Login', () => {
             cy.get("#app > div > main > div > div > div > div > div > div.v-data-table__wrapper > table > tbody > tr:nth-child(1) > td > a")
                 .click()
 
+            // assert url
             cy.url().should('eq', "http://localhost/tickets/8123")
 
+            // assert title
             cy.get("#\\38 123 > div > div > div:nth-child(3) > div:nth-child(2) > div:nth-child(2) > div > div.col-lg-8.col-12 > h1")
                 .should("have.text", " Incident  #8123:  live zebra ")
         })
--- ui/cypress/support/commands.js
+++ ui/cypress/support/commands.js
@@ -1,10 +1,5 @@
 // ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
+// custom commands
 // https://on.cypress.io/custom-commands
 // ***********************************************
 //
