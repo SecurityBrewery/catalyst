96b7a9604c6c581847e5b8ed8bbfb760b85f53b9
Jonas Plum <cugu@users.noreply.github.com>
2024-08-05 15:22:01 +0200

    fix: multi select state handling (#1094)

--- ui/src/components/form/GrowTextarea.vue
+++ ui/src/components/form/GrowTextarea.vue
@@ -1,24 +1,14 @@
 <script setup lang="ts">
-import { Textarea } from '@/components/ui/textarea'
-
-import { useVModel } from '@vueuse/core'
 import { type HTMLAttributes, onMounted, ref } from 'vue'
 
 import { cn } from '@/lib/utils'
 
 const props = defineProps<{
   class?: HTMLAttributes['class']
-  defaultValue?: string | number
-  modelValue?: string | number
 }>()
 
-const emits = defineEmits<{
-  (e: 'update:modelValue', payload: string | number): void
-}>()
-
-const modelValue = useVModel(props, 'modelValue', emits, {
-  passive: true,
-  defaultValue: props.defaultValue
+const modelValue = defineModel<string>({
+  default: ''
 })
 
 const textarea = ref<HTMLElement | null>(null)
